1e4ca83334d5ebae84c4cef14c715dfd844fdf82
rossv <ross.volkwein@gmail.com>
2025-08-09 16:56:43 -0400

    Add loading spinner for data initialization

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import { loadData } from './data.js';
 import { buildUI } from './ui.js';
 
 async function init(){
+  const spinner=document.getElementById('loadingSpinner');
+  spinner.classList.remove('d-none');
   try{
     await loadData();
     buildUI();
@@ -9,6 +11,8 @@ async function init(){
     console.error('Failed to load data',err);
     const sliderTable=document.getElementById('sliderTable');
     sliderTable.innerHTML='<tr><td colspan="5">Data load failed.</td></tr>';
+  }finally{
+    spinner.classList.add('d-none');
   }
 }
 
